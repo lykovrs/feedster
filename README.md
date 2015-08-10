c6313c2cde9cc06e7db38c044848caaa14b64b4d
lykovrs <lykovrs@gmail.com>
2015-08-10 18:18:09 +0300

    create directives

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # feedster
 Feedster
+
+
+Feedster is creating an AngularJS app for their news feed. Click on each post's +1 button.
---
+++ js/directives/feedsterPost.js
@@ -0,0 +1,7 @@
+app.directive('feedsterPost', function(){
+    return {
+        restrict: 'E',
+        scope: 'post',
+        templateUrl: 'js/directives/feedsterPost.html'
+    };
+});
